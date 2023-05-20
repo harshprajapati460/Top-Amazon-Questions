f964d3087b2bd160956cdc2c1fd6f4201a5dc1be
Harsh <115858346+harshprajapati460@users.noreply.github.com>
2023-05-20 21:12:07 +0530

    Added Detect loop in Linked List

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -1,83 +1,47 @@
-// Problem Link : https://leetcode.com/problems/add-two-numbers/
-// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, 
-// and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
-
-// Input: l1 = [2,4,3], l2 = [5,6,4]
-// Output: [7,0,8]
-// Explanation: 342 + 465 = 807.
-
 #include<bits/stdc++.h>
 using namespace std;
-struct ListNode{
-    int data;
-    ListNode *next;
-    ListNode(int x){
-        data = x;
-        next = nullptr;
-    }
 
+// Function to return total valid pairs
+int ValidPairs(int arr[],int n)
+{
 
-};
-// Traverse the two linked lists in order to add preceding zeros in case a list is having lesser digits than the other one.
-// Start from the head node of both lists and call a recursive function for the next nodes.
-// Continue it till the end of the lists.
-// Creates a node for current digits sum and returns the carry
-// // TIME COMPLEXITY O(L)
-ListNode *Add_Numbers(ListNode *l1,ListNode *l2){
-    if(!l1) return l2;
-    if(!l2) return l1;
-    int carry = 0;
-    ListNode *newhead = new ListNode(0);
-    ListNode *temp = newhead;
-    while(l1 != nullptr || l2 != nullptr || carry > 0){
-       int sum = 0;
-       sum += carry;
-        if(l1 != nullptr){
-            sum += l1->data;
-            l1 = l1->next;
-        }
-        if(l2 != nullptr){
-            sum += l2->data;
-            l2 = l2->next;
-        }
-        
-        ListNode *n = new ListNode(sum%10);
-        temp->next = n;
-        temp = temp->next;
-        carry = sum/10;
+	// Initialize count of all the elements
+	int count[121]={0};
 
+	// frequency count of all the elements
+	for(int i=0;i<n;i++)
+		count[arr[i]] += 1;
 
-    }
-    if(carry > 0){
-        ListNode *n = new ListNode(carry);
-        temp->next = n;
-        temp = temp->next;
-    }
-    return newhead->next;
+	int ans = 0;
+	for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+		{
+			if( arr[i] > arr[j])
+				continue;
+			if (arr[i] - arr[j] % 2 == 1)
+				continue;
+
+			// Add total valid pairs
+			ans += count[arr[i]]* count[arr[j]];
+			if (arr[i] == arr[j])
+
+				// Exclude pairs made with a single element
+				// i.e. (x, x)
+				ans -= count[arr[i]];
+		}
+	return ans;
 }
 
-void printList(ListNode *l1){
-    ListNode *temp = l1;
-    while(temp != nullptr){
-        cout<<temp->data<<" ";
-        temp = temp->next;
-    }
-
+// Driver Code
+int main()
+{
+    int n;
+    cin>>n;
+int arr[n];
+for(int i=0; i<n;i++){
+    cin>>arr[i];
 }
-int main(){
-
-ListNode *head1 = new ListNode(3);
-head1->next = new ListNode(4);
-head1->next->next = new ListNode(2);
-
-ListNode *head2 = new ListNode(4);
-head2->next = new ListNode(6);
-head2->next->next = new ListNode(5);
-
-printList(head1);
-cout<<" ";
-printList(head2);
-cout<<endl;
-printList(Add_Numbers(head1,head2));
+// Function call to print required answer
+cout<<(ValidPairs(arr,n));
 return 0;
 }
